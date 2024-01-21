ce5c6badd52b7ab72b3f1b303cfe4248bd51bbcf
IsaiahTheDev-Chromebook <isaiahflorida@gmail.com>
2024-01-21 04:39:09 -0500

    removed text input and button and added layout and some styling

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("bmiForm");
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault(); // Stop page reload from happening
-    const inputData = document.getElementById("userInput").value;
-    console.log("Input data:",inputData);
-    document.getElementById("userInput").value = "";
-})

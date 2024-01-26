bbef99d5222b8ab08d98e67dbbe836a3c639824e
IsaiahTheDev-Chromebook <isaiahflorida@gmail.com>
2024-01-26 18:50:22 -0500

    started working on storing calculated bmi

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const heightSlider = document.getElementById("height-num");
+const heightSlider = document.getElementById("height-slider");
+const heightVal = document.getElementById("height-num");
 const weightValInc = document.getElementById("weight-inc");
 const weightValDec = document.getElementById("weight-dec");
 const weightVal = document.getElementById("weight-num");
@@ -7,15 +8,38 @@ const ageValDec = document.getElementById("age-dec");
 const ageVal = document.getElementById("age-num");
 const maleBtn = document.getElementById("male");
 const femaleBtn = document.getElementById("female");
+const calculate = document.getElementById("calculate");
 
 let height = heightSlider.value;
 let weight = weightVal.innerHTML;
 let age = ageVal.innerHTML;
 let gender = "Male";
+let BMI = 0;
+
+//gender input/control
+const selected = "rgb(12, 15, 28)";
+const notSelected = "rgb(19, 23, 43)";
+maleBtn.style.backgroundColor = selected;
+femaleBtn.style.backgroundColor = notSelected;
+maleBtn.addEventListener("click", () => {
+    if(maleBtn.style.backgroundColor == notSelected) {
+        maleBtn.style.backgroundColor = selected;
+        femaleBtn.style.backgroundColor = notSelected;
+    }
+    gender = "Male";
+});
+femaleBtn.addEventListener("click", () => {
+    if(femaleBtn.style.backgroundColor == notSelected) {
+        femaleBtn.style.backgroundColor = selected;
+        maleBtn.style.backgroundColor = notSelected;
+    }
+    gender = "Female";
+})
 
 //height input/control
 const heightUpdate = () => {
     height = heightSlider.value;
+    heightVal.innerText = height + "'";
 }
 
 heightSlider.addEventListener('change', heightUpdate);
@@ -48,22 +72,8 @@ ageValDec.addEventListener('click', () => {
     ageVal.innerHTML = age;
 });
 
-//gender input/control
-const selected = "rgb(12, 15, 28)";
-const notSelected = "rgb(19, 23, 43)";
-maleBtn.style.backgroundColor = selected;
-femaleBtn.style.backgroundColor = notSelected;
-maleBtn.addEventListener("click", () => {
-    if(maleBtn.style.backgroundColor == notSelected) {
-        maleBtn.style.backgroundColor = selected;
-        femaleBtn.style.backgroundColor = notSelected;
-    }
-    gender = "Male";
-});
-femaleBtn.addEventListener("click", () => {
-    if(femaleBtn.style.backgroundColor == notSelected) {
-        femaleBtn.style.backgroundColor = selected;
-        maleBtn.style.backgroundColor = notSelected;
-    }
-    gender = "Female";
+//calculate button
+calculate.addEventListener('click', () => {
+    BMI = weight/(height*height) * 703
+    console.log('Your BMI is ' + BMI)
 })
